f6c991c38fb838207b493a1fbd00601c4e43bd7c
adrianoelguapo <162363558+adrianoelguapo@users.noreply.github.com>
2025-01-22 18:46:20 +0100

    revertir cagada

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const middlewares = require('./middlewares');
@@ -10,27 +10,24 @@ const middlewares = require('./middlewares');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(helmet({ contentSecurityPolicy: false })); // Configuración más flexible
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://admin:123@cluster0.tz018.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-let client;
+const uri = "mongodb+srv://admin:123@cluster0.tz018.mongodb.net/despliegue_vercel_express?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 let db;
 
-async function connectToDatabase() {
-  if (!client || !client.isConnected()) {
-    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    await client.connect();
+
+client.connect()
+  .then(() => {
     db = client.db('despliegue_vercel_express');
     console.log('Connected to MongoDB Atlas');
-  }
-  return db;
-}
+  })
+  .catch((err) => console.error('Error connecting to MongoDB Atlas:', err));
 
 app.get('/api/users', async (req, res) => {
   try {
-    const db = await connectToDatabase();
     const users = await db.collection('users').find().toArray();
     res.json(users);
   } catch (err) {
@@ -40,12 +37,11 @@ app.get('/api/users', async (req, res) => {
 
 app.get('/api/users/:name', async (req, res) => {
   try {
-    const db = await connectToDatabase();
-    const user = await db.collection('users').findOne({ name: req.params.name });
-    if (!user) {
+    const users = await db.collection('users').find({ name: new ObjectId(req.params.name) });
+    if (!users) {
       return res.status(404).json({ message: 'User not found' });
     }
-    res.json(user);
+    res.json(users);
   } catch (err) {
     res.status(500).json({ message: 'Error retrieving user', error: err });
   }
@@ -53,7 +49,6 @@ app.get('/api/users/:name', async (req, res) => {
 
 app.post('/api/users', async (req, res) => {
   try {
-    const db = await connectToDatabase();
     const newUser = {
       name: req.body.name,
       apellido: req.body.apellido,
@@ -68,9 +63,6 @@ app.post('/api/users', async (req, res) => {
 });
 
 app.use(middlewares.notFound);
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Internal Server Error', error: err.message });
-});
+app.use(middlewares.errorHandler);
 
 module.exports = app;
