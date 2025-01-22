1515f474df06951462c14cb3b01a2ec6277b8a1c
adrianoelguapo <162363558+adrianoelguapo@users.noreply.github.com>
2025-01-22 19:34:52 +0100

    melancolía...

--- api/app.js
+++ api/app.js
@@ -2,57 +2,57 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient } = require('mongodb');
+
 require('dotenv').config();
 
 const middlewares = require('./middlewares');
 
 const app = express();
 
+const url = "mongodb+srv://admin:<db_password>@cluster0.tz018.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const cliente = new MongoClient(url);
+cliente.connect();
+const db = cliente.db('despliegue_vercel_express');
+const coleccion = db.collection('users');
+
+
+
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://admin:123@cluster0.tz018.mongodb.net/despliegue_vercel_express?retryWrites=true&w=majority";
-const client = new MongoClient(url);
-cliente.connect();
-const db = client.db('despliegue_vercel_express');
-const coleccion = db.collection('users');
-
-app.get('/api/users', async (req, res) => {
+app.get("/api/users", async (req, res) =>{
   try {
     const users = await coleccion.find().toArray();
     res.json(users);
-  } catch (err) {
-    res.status(500).json({ message: 'Error retrieving users', error: err });
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener los usuarios' });
   }
 });
 
-app.get('/api/users/:name', async (req, res) => {
+app.get("/api/users/:name", async (req, res) =>{
+  const userId = req.params.name;
   try {
-    const users = await db.coleccion.find({ name: new ObjectId(req.params.name) });
-    if (!users) {
-      return res.status(404).json({ message: 'User not found' });
+    const user = await coleccion.findOne({ name: userId });
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ error: "Usuario no encontrado" });
     }
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ message: 'Error retrieving user', error: err });
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener el usuario' });
   }
 });
 
 app.post('/api/users', async (req, res) => {
+  const { id, nombre, apellido, telefono } = req.body;
   try {
-    const newUser = {
-      name: req.body.name,
-      apellido: req.body.apellido,
-      tlf: req.body.tlf,
-    };
-
-    const result = await db.coleccion.insertOne(newUser);
-    res.status(201).json(newUser);
-  } catch (err) {
-    res.status(400).json({ message: 'Error creating user', error: err });
+    await coleccion.insertOne({ id, nombre, apellido, telefono });
+    res.status(201).json({ message: 'Usuario añadido exitosamente.', user: { id, nombre, apellido, telefono } });
+  } catch (error) {
+    res.status(500).json({ error: 'Error al añadir el usuario' });
   }
 });
 
